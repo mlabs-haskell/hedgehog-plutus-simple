fc6238de6bbc4c317a91de0ae17b848e833c4914
nalane <nathaniel.patrick.lane@gmail.com>
2021-10-07 11:25:16 -0600

    Getting cmd line arguments

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get the project and class names from command line args
+# Probably want to make it so that we only need to provide one later
+if [ $# != 2 ]
+  then
+    echo "Run this with two args: the project name and the class name."
+    echo "E.g.: ${0} liquidity-bridge LiquidityBridge"
+    exit 1
+fi
+PROJECTNAME=$1
+CLASSNAME=$2
+
+mv liquidity-bridge.cabal "${PROJECTNAME}.cabal"
