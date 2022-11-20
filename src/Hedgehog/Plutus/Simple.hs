101582b748d7d3e2893d20b27b0680a726a824be
Ari Fordsham <arifordsham@gmail.com>
2022-11-20 17:39:25 +0200

    Formatting

--- src/Hedgehog/Plutus/Simple.hs
+++ src/Hedgehog/Plutus/Simple.hs
@@ -1,22 +1,40 @@
-module Hedgehog.Plutus.Simple(
+module Hedgehog.Plutus.Simple (
   testNoErrors,
   testNoErrorsTrace,
-  testLimits
-  ) where
+  testLimits,
+) where
 
-import PlutusLedgerApi.V1.Value(Value)
-import Plutus.Model(MockConfig, Run, checkErrors, runMock, initMock, Mock, ppMockEvent, mockNames, getLog, Log, TxStat, warnLimits, ppLimitInfo, mockTxs)
+import Plutus.Model (
+  Log,
+  Mock,
+  MockConfig,
+  Run,
+  TxStat,
+  checkErrors,
+  getLog,
+  initMock,
+  mockNames,
+  mockTxs,
+  ppLimitInfo,
+  ppMockEvent,
+  runMock,
+  warnLimits,
+ )
+import PlutusLedgerApi.V1.Value (Value)
 
-import Hedgehog(success, footnote, failure, PropertyT, evalIO, assert)
+import Data.Kind (Type)
 import Data.Maybe (isJust, isNothing)
+import Hedgehog (PropertyT, assert, evalIO, failure, footnote, success)
 
-testNoErrors :: Value -> MockConfig -> Run a -> PropertyT IO ()
+testNoErrors ::
+  forall (a :: Type). Value -> MockConfig -> Run a -> PropertyT IO ()
 testNoErrors funds cfg act =
   case fst (runAction funds cfg act) of
-    Nothing -> success 
-    Just err -> footnote err >> failure 
+    Nothing -> success
+    Just err -> footnote err >> failure
 
-testNoErrorsTrace :: Value -> MockConfig -> Run a -> PropertyT IO ()
+testNoErrorsTrace ::
+  forall (a :: Type). Value -> MockConfig -> Run a -> PropertyT IO ()
 testNoErrorsTrace funds cfg act = do
   evalIO $ do
     putStrLn ""
@@ -27,14 +45,22 @@ testNoErrorsTrace funds cfg act = do
   where
     (errors, bch) = runAction funds cfg act
 
-testLimits :: Value -> MockConfig -> (Log TxStat -> Log TxStat) -> Run a -> PropertyT IO ()
+testLimits ::
+  forall (a :: Type).
+  Value ->
+  MockConfig ->
+  (Log TxStat -> Log TxStat) ->
+  Run a ->
+  PropertyT IO ()
 testLimits initFunds cfg tfmLog act =
   if isNothing errors
     then success
-    else footnote (ppLimitInfo (mockNames bch) (tfmLog $ mockTxs bch)) >> failure
+    else
+      footnote (ppLimitInfo (mockNames bch) (tfmLog $ mockTxs bch))
+        >> failure
   where
     (errors, bch) = runAction initFunds (warnLimits cfg) act
 
-
-runAction :: Value -> MockConfig -> Run a -> (Maybe String, Mock)
+runAction ::
+  forall (a :: Type). Value -> MockConfig -> Run a -> (Maybe String, Mock)
 runAction funds cfg act = runMock (act >> checkErrors) (initMock cfg funds)
