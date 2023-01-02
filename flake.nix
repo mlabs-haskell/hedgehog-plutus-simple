485e26651c7578fb743b212f1fa68968a6dedc1b
Geometer1729 <16kuhnb@gmail.com>
2023-01-02 13:20:29 -0500

    update psm

--- flake.nix
+++ flake.nix
@@ -12,10 +12,11 @@
   inputs = {
     tooling.url = "github:mlabs-haskell/mlabs-tooling.nix";
 
-    plutus-simple-model.url = "github:mlabs-haskell/plutus-simple-model/fa0aa0382ecabf6fcbef8c7b9c35d85ff7b57765";
+    plutus-simple-model.url = "github:mlabs-haskell/plutus-simple-model";
+    plutarch.url = "github:plutonomicon/plutarch-plutus";
   };
 
-  outputs = inputs@{ self, tooling, plutus-simple-model, ... }: tooling.lib.mkFlake { inherit self; }
+  outputs = inputs@{ self, tooling, plutus-simple-model, plutarch, ... }: tooling.lib.mkFlake { inherit self; }
     {
       imports = [
         (tooling.lib.mkHaskellFlakeModule1 {
@@ -23,6 +24,7 @@
 
           project.extraHackage = [
             "${plutus-simple-model}"
+            "${plutarch}"
           ];
         })
       ];
