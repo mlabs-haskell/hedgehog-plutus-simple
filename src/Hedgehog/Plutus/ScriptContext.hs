fe2cebf44cfbb9db3bafd347e88aadd55ff3b38f
Ari Fordsham <arifordsham@gmail.com>
2023-02-07 16:08:05 +0000

    Progress

--- src/Hedgehog/Plutus/ScriptContext.hs
+++ src/Hedgehog/Plutus/ScriptContext.hs
@@ -4,4 +4,31 @@ module Hedgehog.Plutus.ScriptContext where
 
 import Data.Kind (Type)
 
-data Purpose = Spending Type | Minting
+import PlutusLedgerApi.V2 qualified as Plutus
+
+import Hedgehog.Plutus.Adjunction
+
+data ScriptType = Spend Type | Mint | Reward | Certify
+
+type DatumOf :: ScriptType -> Type
+type family DatumOf st where
+  DatumOf ('Spend d) = d
+  DatumOf _ = ()
+
+type ScriptPurpose :: ScriptType -> Type
+data ScriptPurpose st where
+  Spending :: Plutus.TxOutRef -> ScriptPurpose ('Spend d)
+  Minting :: Plutus.CurrencySymbol -> ScriptPurpose 'Mint
+  Rewarding :: Plutus.StakingCredential -> ScriptPurpose 'Reward
+  Certifying :: Plutus.DCert -> ScriptPurpose 'Certify
+
+type ScriptContext :: Type -> ScriptType -> Type
+data ScriptContext redeemer st = ScriptContext
+  { contextRedeemer :: redeemer
+  , contextDatum :: DatumOf st
+  , contextPurpose :: ScriptPurpose st
+  , contextTxInfo :: Plutus.TxInfo
+  }
+
+scriptContext :: Adjunction (ScriptContext d st) _
+scriptContext = _
