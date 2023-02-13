b6739a6ea5f42a54bf7f1e3dcd43d934f8b35cda
Geometer1729 <16kuhnb@gmail.com>
2023-02-13 15:27:27 -0500

    start script context adjunction

--- src/Hedgehog/Plutus/TxTest.hs
+++ src/Hedgehog/Plutus/TxTest.hs
@@ -1,38 +1,46 @@
 {-# LANGUAGE TypeFamilyDependencies #-}
+-- TODO can this be just for Prelude?
+{-# OPTIONS_GHC -Wno-missing-import-lists #-}
 
-{- HLINT ignore "Redundant bracket" -}
 module Hedgehog.Plutus.TxTest (
-  TxTest (TxTest),
+  TxTest,
   txTest,
   omitted,
+  resolveOmitted,
   txTestBadAdjunction,
   txTestGoodAdjunction,
   txTestBad,
   txTestGood,
 ) where
 
+import Prelude hiding ((.))
+
+import Data.Map qualified as Map
+import Data.Set qualified as Set
+
+import Control.Category (Category ((.)))
+
+import Prelude hiding ((.))
+
 import PlutusLedgerApi.V2 qualified as Plutus
 
 import Plutus.Model qualified as Model
 
-import Hedgehog qualified
-
-import Hedgehog.Plutus.Adjunction (
-  Adjunction (lower),
-  adjunctionTest,
- )
+import Cardano.Simple.Ledger.Tx (Tx (..), TxIn (..))
+import Hedgehog.Plutus.Adjunction (Adjunction (..), adjunctionTest)
 import Hedgehog.Plutus.ScriptContext (
   DatumOf,
-  ScriptContext,
-  ScriptTx,
-  scriptTxValid,
+  ScriptContext (..),
+  ScriptTx (..), scriptTxValid,
  )
 import Hedgehog.Plutus.TestData (
   Bad,
+  --Generalised,
   Good,
   TestData,
   testDataAdjunction,
  )
+import qualified Hedgehog
 
 newtype TxTest st a
   = TxTest
@@ -75,13 +83,100 @@ scriptContext ::
   Model.Mock ->
   DatumOf st ->
   Adjunction (ScriptTx st) (ScriptContext r st)
-scriptContext = _
+scriptContext _ _ =
+  Adjunction
+    { lower =
+        \( ScriptContext
+            { contextTxInfo = _
+            , contextPurpose
+            , contextRedeemer = _
+            }
+          ) ->
+            ScriptTx
+              { scriptTxPurpose = contextPurpose
+              , scriptTx =
+                  Model.Tx
+                    (error "TODO extra")
+                    $ Tx
+                      -- TODO is there a better option than construction the tx by hand here?
+                      (error "TODO")
+                      (error "TODO")
+                      (error "TODO")
+                      (error "TODO")
+                      (error "TODO")
+                      (error "TODO")
+                      (error "TODO")
+                      (error "TODO")
+                      (error "TODO")
+                      (error "TODO")
+                      (error "TODO")
+                      (error "TODO")
+                      (error "TODO")
+                      (error "TODO")
+              }
+    , raise =
+        ( \( ScriptTx
+              { scriptTxPurpose
+              , scriptTx =
+                Model.Tx
+                  { Model.tx'plutus =
+                    Tx
+                      { txInputs
+                      , txReferenceInputs
+                      , txOutputs
+                      , txFee
+                      , txMint
+                      , txValidRange
+                      , txSignatures
+                      , txRedeemers
+                      , txData
+                      -- ,txId
+                      }
+                  }
+              }
+            ) ->
+              let
+                convertIn :: TxIn -> Plutus.TxInInfo
+                convertIn = error "TODO"
+                adaToValue :: Model.Ada -> Plutus.Value
+                adaToValue = error "TODO"
+               in
+                ScriptContext
+                  { contextRedeemer = error "TODO" $ txRedeemers
+                  , -- TODO get the script from the script purpouse
+                    -- lookup the reddeemer
+                    -- use fromData
+                    contextPurpose = scriptTxPurpose
+                  , contextTxInfo =
+                      Plutus.TxInfo
+                        { Plutus.txInfoInputs = convertIn <$> Set.toList txInputs
+                        , Plutus.txInfoReferenceInputs = convertIn <$> Set.toList txReferenceInputs
+                        , Plutus.txInfoOutputs = txOutputs
+                        , Plutus.txInfoFee = adaToValue txFee
+                        , Plutus.txInfoMint = txMint
+                        , Plutus.txInfoDCert = error "TODO"
+                        , Plutus.txInfoWdrl = error "TODO"
+                        , Plutus.txInfoValidRange = error "TODO" $ txValidRange
+                        , Plutus.txInfoSignatories = Map.keys txSignatures
+                        , Plutus.txInfoRedeemers = error "TODO" $ txRedeemers
+                        , Plutus.txInfoData = error "TODO" $ txData
+                        , Plutus.txInfoId = error "TODO"
+                        }
+                  }
+        )
+    }
 
 omitted :: Plutus.TxId
 omitted = error "You shouldn't read this"
 
 resolveOmitted :: Model.Mock -> datum -> Plutus.TxInfo -> Plutus.TxInfo
-resolveOmitted = _
+resolveOmitted _mock _d txinfo =
+  txinfo
+    { Plutus.txInfoInputs = []
+    , Plutus.txInfoReferenceInputs = []
+    , Plutus.txInfoOutputs = []
+    , Plutus.txInfoData = error "TODO"
+    }
 
 txTestBadAdjunction ::
   ( Hedgehog.MonadTest m
