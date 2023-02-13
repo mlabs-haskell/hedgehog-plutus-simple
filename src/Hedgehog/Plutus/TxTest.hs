fe3e78cf8a1c2161ef5d7bf5ff3b1c267b3962ce
Geometer1729 <16kuhnb@gmail.com>
2023-02-13 16:53:11 -0500

    clean up code a bit work on redeemer ptr stuff

--- src/Hedgehog/Plutus/TxTest.hs
+++ src/Hedgehog/Plutus/TxTest.hs
@@ -10,22 +10,27 @@ module Hedgehog.Plutus.TxTest (
 
 import Prelude hiding ((.))
 
+import Control.Arrow (first)
 import Data.Kind (Constraint, Type)
 import Data.Map qualified as Map
+import Data.Maybe (fromMaybe)
 import Data.Set qualified as Set
 
 import Control.Category (Category ((.)))
 
 import PlutusLedgerApi.V2 qualified as Plutus
+import PlutusLedgerApi.V2.Tx qualified as Plutus
 import PlutusTx.AssocMap qualified
 
 import Plutus.Model qualified as Model
+import PlutusTx.AssocMap qualified as PlutusTx
 
 import Cardano.Simple.Ledger.Tx (Tx (..), TxIn (..))
 import Hedgehog.Plutus.Adjunction (Adjunction (..))
 import Hedgehog.Plutus.ScriptContext (
   DatumOf,
   ScriptContext (..),
+  ScriptPurpose (..),
   ScriptTx (..),
  )
 import Hedgehog.Plutus.TestData (
@@ -60,7 +65,7 @@ details, which will be supplied for you:
 For the final three, you can use the 'omitted' function to signal this.
 -}
 txTest ::
-  (TestData a) =>
+  (TestData a, Plutus.FromData r) =>
   ( Model.Mock ->
     DatumOf st ->
     Adjunction (ScriptContext r st) (Generalised a)
@@ -72,91 +77,110 @@ txTest f = TxTest $ \mock datum ->
     . scriptContext mock datum
 
 scriptContext ::
+  forall st r.
+  Plutus.FromData r =>
   Model.Mock ->
   DatumOf st ->
   Adjunction (ScriptTx st) (ScriptContext r st)
-scriptContext _ _ =
-  Adjunction
-    { lower =
-        \( ScriptContext
-            { contextTxInfo = _
-            , contextPurpose
-            , contextRedeemer = _
-            }
-          ) ->
-            ScriptTx
-              { scriptTxPurpose = contextPurpose
-              , scriptTx =
-                  Model.Tx
-                    (error "TODO extra")
-                    $ Tx
-                      -- TODO is there a better option than construction the tx by hand here?
-                      (error "TODO")
-                      (error "TODO")
-                      (error "TODO")
-                      (error "TODO")
-                      (error "TODO")
-                      (error "TODO")
-                      (error "TODO")
-                      (error "TODO")
-                      (error "TODO")
-                      (error "TODO")
-                      (error "TODO")
-                      (error "TODO")
-                      (error "TODO")
-                      (error "TODO")
-              }
-    , raise =
-        ( \( ScriptTx
-              { scriptTxPurpose
-              , scriptTx =
+scriptContext
+  Model.Mock {Model.mockUtxos = utxos}
+  _ =
+    Adjunction {lower, raise}
+    where
+      lower :: ScriptContext r st -> ScriptTx st
+      lower
+        ScriptContext
+          { contextTxInfo = _
+          , contextPurpose
+          , contextRedeemer = _
+          } =
+          ScriptTx
+            { scriptTxPurpose = contextPurpose
+            , scriptTx =
                 Model.Tx
-                  { Model.tx'plutus =
-                    Tx
-                      { txInputs
-                      , txReferenceInputs
-                      , txOutputs
-                      , txFee
-                      , txMint
-                      , txValidRange
-                      , txSignatures
-                      , txRedeemers
-                      , txData
-                      -- ,txId
-                      }
+                  (error "TODO extra")
+                  $ Tx
+                    -- TODO is there a better option than construction the tx by hand here?
+                    (error "TODO")
+                    (error "TODO")
+                    (error "TODO")
+                    (error "TODO")
+                    (error "TODO")
+                    (error "TODO")
+                    (error "TODO")
+                    (error "TODO")
+                    (error "TODO")
+                    (error "TODO")
+                    (error "TODO")
+                    (error "TODO")
+                    (error "TODO")
+                    (error "TODO")
+            }
+      raise :: ScriptTx st -> ScriptContext r st
+      raise
+        ScriptTx
+          { scriptTxPurpose
+          , scriptTx =
+            Model.Tx
+              { Model.tx'extra = _
+              , Model.tx'plutus =
+                Tx
+                  { txInputs
+                  , txReferenceInputs
+                  , txOutputs
+                  , txFee
+                  , txMint
+                  , txValidRange
+                  , txSignatures
+                  , txRedeemers
+                  , txData
+                  -- ,txId
                   }
               }
-            ) ->
-              let
-                convertIn :: TxIn -> Plutus.TxInInfo
-                convertIn = error "TODO"
-                adaToValue :: Model.Ada -> Plutus.Value
-                adaToValue = error "TODO"
-               in
-                ScriptContext
-                  { contextRedeemer = error "TODO" $ txRedeemers
-                  , -- TODO get the script from the script purpouse
-                    -- lookup the reddeemer
-                    -- use fromData
-                    contextPurpose = scriptTxPurpose
-                  , contextTxInfo =
-                      Plutus.TxInfo
-                        { Plutus.txInfoInputs = convertIn <$> Set.toList txInputs
-                        , Plutus.txInfoReferenceInputs = convertIn <$> Set.toList txReferenceInputs
-                        , Plutus.txInfoOutputs = txOutputs
-                        , Plutus.txInfoFee = adaToValue txFee
-                        , Plutus.txInfoMint = txMint
-                        , Plutus.txInfoDCert = error "TODO"
-                        , Plutus.txInfoWdrl = error "TODO"
-                        , Plutus.txInfoValidRange = error "TODO" $ txValidRange
-                        , Plutus.txInfoSignatories = Map.keys txSignatures
-                        , Plutus.txInfoRedeemers = error "TODO" $ txRedeemers
-                        , Plutus.txInfoData = error "TODO" $ txData
-                        , Plutus.txInfoId = error "TODO"
-                        }
-                  }
-        )
-    }
+          } =
+          let
+            convertIn :: TxIn -> Plutus.TxInInfo
+            convertIn TxIn {txInRef} = Plutus.TxInInfo txInRef out
+              where
+                out =
+                  fromMaybe (error "lookup failure") $
+                    Map.lookup txInRef utxos
+
+            adaToValue :: Model.Ada -> Plutus.Value
+            adaToValue = error "TODO"
+            redeemerPtr :: Plutus.RedeemerPtr
+            redeemerPtr = error "TODO"
+            -- TODO how can I get this?
+            -- is this all wrong and I can get the redeemer from Extra?
+            redeemer :: Plutus.Redeemer
+            redeemer =
+              fromMaybe (error "redeemer ptr not found") $
+                Map.lookup redeemerPtr txRedeemers
+            redeemerData :: Plutus.BuiltinData
+            redeemerData = case redeemer of
+              Plutus.Redeemer d -> d
+           in
+            ScriptContext
+              { contextRedeemer =
+                  fromMaybe (error "failed to parse redeemer") $
+                    Plutus.fromBuiltinData redeemerData
+              , contextPurpose = scriptTxPurpose
+              , contextTxInfo =
+                  Plutus.TxInfo
+                    { Plutus.txInfoInputs = convertIn <$> Set.toList txInputs
+                    , Plutus.txInfoReferenceInputs = convertIn <$> Set.toList txReferenceInputs
+                    , Plutus.txInfoOutputs = txOutputs
+                    , Plutus.txInfoFee = adaToValue txFee
+                    , Plutus.txInfoMint = txMint
+                    , Plutus.txInfoDCert = error "TODO"
+                    , Plutus.txInfoWdrl = error "TODO"
+                    , Plutus.txInfoValidRange = error "TODO" $ txValidRange
+                    , Plutus.txInfoSignatories = Map.keys txSignatures
+                    , Plutus.txInfoRedeemers = PlutusTx.fromList $ map (first (error "TODO")) $ Map.toList $ txRedeemers
+                    , Plutus.txInfoData = PlutusTx.fromList $ Map.toList txData
+                    , Plutus.txInfoId = error "TODO"
+                    }
+              }
 
 type Omittable :: Type -> Constraint
 class Omittable a
