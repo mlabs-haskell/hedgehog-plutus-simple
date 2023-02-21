cc91bd3dc86753d9e203833f490ffcd5656cb0b4
Geometer1729 <16kuhnb@gmail.com>
2023-02-21 12:13:26 -0500

    fix lint

--- src/Hedgehog/Plutus/TxTest.hs
+++ src/Hedgehog/Plutus/TxTest.hs
@@ -599,7 +599,7 @@ resolveOmitted mock@Model.Mock {Model.mockUtxos = utxos} scripts mps d txinfo =
                 . convertIn' mock redeemers scripts datums
                 <$> Plutus.txInfoInputs txinfo
           ]
-            ++ Plutus.txInfoSignatories txinfo
+            <> Plutus.txInfoSignatories txinfo
     , Plutus.txInfoRedeemers = redeemers
     , Plutus.txInfoData = datums
     }
