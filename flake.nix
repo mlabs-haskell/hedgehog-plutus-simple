132af0259080cb6946a9244c088724b26fd78f3b
Geometer1729 <16kuhnb@gmail.com>
2023-02-21 16:21:58 -0500

    start adding pioneer inputs

--- flake.nix
+++ flake.nix
@@ -13,10 +13,16 @@
     tooling.url = "github:mlabs-haskell/mlabs-tooling.nix";
     plutus-simple-model.url = "github:mlabs-haskell/plutus-simple-model/";
     plutarch.url = "github:Plutonomicon/plutarch-plutus/?rev=f535a6894a25e6d46d16958273769bffa8880090";
+    pioneer.url = "github:input-output-hk/plutus-pioneer-program/?ref=third-iteration";
+    pioneer.flake = false;
+    plutus.url = "github:input-output-hk/plutus";
+    plutus.follows = "tooling";
+    plutus-apps.url = "github:input-output-hk/plutus-apps/?rev=41149926c108c71831cfe8d244c83b0ee4bf5c8a";
+
     # plutarch has a parse error on the newest commit right now
   };
 
-  outputs = inputs@{ self, tooling, plutus-simple-model, plutarch, ... }: tooling.lib.mkFlake { inherit self; }
+  outputs = inputs@{ self, tooling, plutus-simple-model, plutarch, pioneer, plutus-apps, ... }: tooling.lib.mkFlake { inherit self; }
     {
       imports = [
         (tooling.lib.mkHaskellFlakeModule1 {
@@ -26,6 +32,10 @@
             "${plutarch}"
             "${plutus-simple-model}/psm"
             "${plutus-simple-model}/cardano-simple"
+            "${pioneer}/code/week01/"
+            "${plutus-apps}/plutus-ledger-constraints/"
+            "${plutus-apps}/plutus-ledger/"
+            "${plutus-apps}/freer-extras/"
           ];
           toHaddock = [
             "plutarch"
