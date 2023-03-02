7578497545029f63c1a62d9980ee960908decb90
Geometer1729 <16kuhnb@gmail.com>
2023-03-02 15:05:44 -0500

    scriptTxValid

--- src/Hedgehog/Plutus/ScriptContext.hs
+++ src/Hedgehog/Plutus/ScriptContext.hs
@@ -15,6 +15,10 @@ import Data.Kind (Type)
 
 import PlutusLedgerApi.V2 qualified as Plutus
 
+import Cardano.Simple.Ledger.Tx (Tx (..), TxIn (..))
+import Control.Monad (forM_, void)
+import Data.Map qualified as Map
+import Data.Maybe (isJust)
 import Plutus.Model qualified as Model
 
 data ScriptTx st = ScriptTx
@@ -48,7 +52,39 @@ data ScriptContext redeemer st = ScriptContext
   }
 
 scriptTxValid :: ScriptTx st -> Model.Mock -> Bool
-scriptTxValid = _
+scriptTxValid
+  ScriptTx
+    { scriptTx =
+      Model.Tx
+        { Model.tx'plutus =
+          Tx
+            { txInputs
+            , txReferenceInputs
+            , txSignatures
+            , txData
+            }
+        , Model.tx'extra = _extra
+        }
+    , scriptTxPurpose
+    }
+  Model.Mock
+    { Model.mockUtxos = utxos
+    , Model.mockUsers = users
+    , Model.mockDatums = allDatums
+    } =
+    isJust $ do
+      forM_ txInputs validateIn
+      forM_ txReferenceInputs validateIn
+      forM_ (Map.keys txSignatures) validatePkh
+      forM_ (Map.keys txData) validateDh
+      case scriptTxPurpose of
+        Spending ref -> void $ validateRef ref
+        _ -> pure ()
+    where
+      validateIn TxIn {txInRef} = validateRef txInRef
+      validateRef ref = Map.lookup ref utxos
+      validatePkh pkh = Map.lookup pkh users
+      validateDh dh = Map.lookup dh allDatums
 
 plutusScriptContext :: ScriptContext d st -> Plutus.ScriptContext
 plutusScriptContext
